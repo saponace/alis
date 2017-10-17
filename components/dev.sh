1e9c4bd4e6a97a31231bb0b0f20e5c3b2ebb4571
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2017-10-17 22:35:53 +0200

    Removed useless dev packages

--- components/dev.sh
+++ components/dev.sh
@@ -3,17 +3,12 @@
 #-------------------------------------------------
 
 
-# Texlive-most (LaTeX compiler)
-    ${INSTALL} texlive-most
 # Java Development Kit
     ${INSTALL} jdk7-openjdk openjdk7-doc
     ${INSTALL} jdk8-openjdk openjdk8-doc
     # Make JRE the default JRE
         sudo su -c "echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk' >> /etc/environment"
-# C debug tools
-    ${INSTALL} gdb
 # Java build tools
     ${INSTALL} maven
 # IDEs
     ${INSTALL} intellij-idea-ultimate-edition
-    ${INSTALL} webstorm
