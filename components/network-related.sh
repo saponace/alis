4b286d84a5f3892817fa8708cb5c14282e4160f1
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2017-10-28 17:54:23 +0200

    Removed old obsolete VPN init code (use different library now)

--- components/network-related.sh
+++ components/network-related.sh
@@ -16,24 +16,3 @@
         sudo systemctl enable NetworkManager.service
     # Disable ipv6 in dhcpcd.conf
         su -c "echo -e 'noipv6rs\nnoipv6' >> /etc/dhcpcd.conf"
-
-
-# Configure VPN (auto generate OpenVPN config files for VPN Private Internet Access)
-    ${INSTALL} openvpn
-    ${INSTALL} private-internet-access-vpn
-    # Write credentials in a file
-        echo "Enter VPN login"
-        read VPN_LOGIN
-        echo "Enter VPN password"
-        read -s VPN_PASSWORD
-        vpn_credentials_file="etc/private-internet-access/login.conf"
-
-        sudo echo ${VPN_LOGIN} > ${vpn_credentials_file}
-        sudo echo ${VPN_PASSWORD} >> ${vpn_credentials_file}
-
-        unset VPN_LOGIN
-        unset VPN_PASSWORD
-        sudo chown root:root ${vpn_credentials_file}
-        sudo chmod 600 ${vpn_credentials_file}
-    # Create OpenVPN config files
-        sudo pia -a
