cd57b274645f9336a3b0aec8768974d246a44040
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2019-03-18 22:03:32 +0100

    Feat: update dev toolkits versions

--- components/dev.sh
+++ components/dev.sh
@@ -4,8 +4,8 @@
 
 
 # Java Development Kit
-    ${INSTALL} jdk7-openjdk openjdk7-doc
-    ${INSTALL} jdk8-openjdk openjdk8-doc
+    ${INSTALL} jdk8-openjdk openjdk8-doc openjdk8-src
+    ${INSTALL} jdk10-openjdk openjdk10-doc openjdk10-src
     # Make JRE the default JRE
         sudo su -c "echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk' >> /etc/environment"
 # Java build tools
