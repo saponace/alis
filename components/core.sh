647f40f493e8336d4479b84614c5df45d74e9df5
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2019-04-28 18:39:07 +0200

    fix: Set X11 keymap

--- components/core.sh
+++ components/core.sh
@@ -7,6 +7,7 @@
     ${INSTALL} linux-headers
 # Set locale and keyboard layout in the terminal and X
     sudo localectl set-keymap ${keymap}
+    sudo localectl set-x11-keymap ${x11_keymap}
 # Disable terminal bell (actually totally disable internal speaker)
     sudo su -c "echo 'blacklist pcspkr' > /etc/modprobe.d/nobeep.conf"
 # Network Time Protocl Daemon synchronization( synchronize time with internet)
