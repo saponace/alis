d4f6a8596cd984a0ce9cda6df2bf21539d303b74
Remi Somdecoste-Lespoune <saponace@gmail.com>
2019-05-08 18:59:24 +0000

    fix: Made pypia able to access NetworkManager connections directory

--- components/vpn-client.sh
+++ components/vpn-client.sh
@@ -3,10 +3,14 @@
 #-------------------------------------------------
 
 
-${INSTALL} python3
+${INSTALL} python3 python-pip
 sudo pip install pypia
 
 # Install Networkmanager applet and all PIA servers as VPN entries in Networkmanager
     pypia -i
+
+# Prepare NetworkManager connections directory to be readable by all (pypia needs read access to that directory)
+    sudo mkdir -p /etc/NetworkManager/system-connections/
+    sudo chmod a+r /etc/NetworkManager/system-connections/
 
 # WIll then need to manually configure pypia (validate packages installation and input PIA creadentials)
