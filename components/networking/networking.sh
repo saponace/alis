12dfbd38e7aa82386ba8d0be86fdf468c914e062
Remi Somdecoste-Lespoune <saponace@gmail.com>
2021-11-26 18:58:28 -0500

    chore: adapt "networking" component to manjaro

--- components/networking/networking.sh
+++ components/networking/networking.sh
@@ -1,22 +1,11 @@
 #-------------------------------------------------
-# Install/configure networking related programs
+# Configure networking
 #-------------------------------------------------
 
-
-# Install Networkmanager packages
-    # To access network state without root privileges
-        ${INSTALL} wpa_supplicant
-    # Network manager
-        ${INSTALL} networkmanager
-    # NetworkManager tray icon
-        ${INSTALL} network-manager-applet gnome-keyring gnome-icon-theme
-
 # Configure Networkmanager
-    # Enable network manager
-        sudo systemctl enable NetworkManager.service
     # Disable ipv6 in dhcpcd.conf
         sudo su -c "echo -e 'noipv6rs\nnoipv6' >> /etc/dhcpcd.conf"
-    # Disable NetworkManager /etc/resolv.conf DNS IP address dynamic updates
-    # to ISP own DNS servers and manually set custom DNS servers IPs
+    # Disable NetworkManager /etc/resolv.conf DNS automatic updates and manually set custom DNS servers
+    # If not disabled; NetworkManager will set arbitrary DNS (notably ISP DNS)
         sudo su -c "echo -e '[main]\ndns=none' >> /etc/NetworkManager/NetworkManager.conf"
-        create_link components/networking/resolv.conf /etc/resolv.conf_static_manual_fallback
+        create_link components/networking/config/resolv.conf /etc/resolv.conf_static_manual_fallback
