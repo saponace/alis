9abc22774cb64e162ae87a487c804a2e963b347c
Remi Somdecoste-Lespoune <saponace@gmail.com>
2021-11-26 19:43:26 -0500

    chore: remove obsolete packages in component "display" after migration to manjaro

--- components/display/display.sh
+++ components/display/display.sh
@@ -1,10 +1,7 @@
 #-------------------------------------------------
-# Install display dependencies
+# Display management
 #-------------------------------------------------
 
-
-# X server
-    ${INSTALL} xorg-server xorg-server-utils xorg-xinit
 # Multi-monitor management
     ${INSTALL} xrandr arandr
 # Color shifter, reduce blue emission
