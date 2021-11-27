6c8858d9559e092091655196d7a1223421d60865
Remi Somdecoste-Lespoune <saponace@gmail.com>
2021-11-26 19:24:03 -0500

    chore: removed obsolete code

--- components/dev.sh
+++ components/dev.sh
@@ -4,19 +4,11 @@
 
 
 # Java Development Kit
-    ${INSTALL} jdk8-openjdk openjdk8-doc openjdk8-src
-    ${INSTALL} jdk11-openjdk openjdk11-doc openjdk11-src
+    # Installing this JDK first will set it qs default JDK
     ${INSTALL} jdk-openjdk openjdk-doc openjdk-src
-    # Make JRE17 the default JRE
-        sudo archlinux-java set java-17-openjdk
+    ${INSTALL} jdk11-openjdk openjdk11-doc openjdk11-src
+    ${INSTALL} jdk8-openjdk openjdk8-doc openjdk8-src
 # Java build tools
     ${INSTALL} maven
 # IDEs
     ${INSTALL} intellij-idea-ultimate-edition
-
-# Google app script command line client
-    # ${INSTALL} npm
-    # sudo npm install @google/clasp
-
-# Node Version Manager
-    # ${INSTALL} nvm
