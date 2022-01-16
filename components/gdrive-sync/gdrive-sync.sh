ea15f4268f59eee82ec207e7d522ef3b95f51528
Remi Somdecoste-Lespoune <saponace@gmail.com>
2022-01-16 17:28:50 -0500

    feat: Mount content of Google drive on /mnt/google-drive

---
+++ components/gdrive-sync/gdrive-sync.sh
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+# Synchronize files between Google drive and this local machine
+#-------------------------------------------------
+
+
+# Install sync utility
+    ${INSTALL} rclone
+
+# Auto-mount the distant directro at startup
+    mkdir -p /mnt/google-drive
+    create_link ${USER_HOMEDIR_DOTFILES_DESTINATION}/.config/rclone /etc  # Systemd unit file looks for configuration in /etc/rclone
+    create_link components/gdrive-sync/gdrive-sync.service /etc/systemd/system/
+    sudo systemctl enable gdrive-sync.service
+
+
+# MANUAL-TODO: Create a connection to Google drive: `rclone config`. Name the remote "gdrive"
+
+# TODO: When rclone version 1.58 is released, use `rclone bisync` instead of `rclone mount`
