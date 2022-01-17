dd98be9aba16d68bf5a60d6adc3df7c9cd2e204d
Remi Somdecoste-Lespoune <saponace@gmail.com>
2022-01-16 19:18:14 -0500

    feat: add autorandr to save xrandr layout and re-apply them automatically

--- components/display/display.sh
+++ components/display/display.sh
@@ -2,8 +2,8 @@
 # Display management
 #-------------------------------------------------
 
-# Multi-monitor management
-    ${INSTALL} xrandr arandr
+# Multi-monitor layout management
+    ${INSTALL} xrandr arandr autorandr
 # Color shifter, reduce blue emission
     ${INSTALL} redshift
     create_link components/display/config/redshift.conf ${USER_HOMEDIR_DOTFILES_DESTINATION}/.config
