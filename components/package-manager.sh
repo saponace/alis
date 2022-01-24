af53018b82a3b034139c001c9281ac0274936f71
Remi Somdecoste-Lespoune <saponace@gmail.com>
2022-01-23 19:29:40 -0500

    fix: force pacman local database sync before installing any package

--- components/package-manager.sh
+++ components/package-manager.sh
@@ -4,6 +4,9 @@
 
 # Yay AUR helper should be pre-intalle. No need to install another AUR helper
 
+# Sync packages and force refresh of package database
+    sudo pacman -Syy
+
 # Configure pacman
     sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
     sudo sed -i 's/#ParallelDownloads.*/ParallelDownloads = 5/g' /etc/pacman.conf
