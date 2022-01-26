fa47140e158854b8889b15bf7287c4fa4d7a8df5
Remi Somdecoste-Lespoune <saponace@gmail.com>
2022-01-25 23:31:24 -0500

    feat: easy-to-use (audio) bluetooth

--- components/hardware-drivers.sh
+++ components/hardware-drivers.sh
@@ -4,6 +4,12 @@
 
 
 # Video drivers
-    # TODO: Check if xf86-video-vesa is reauired, and if other open source drivers should be installed on a
+    # TODO: Check if xf86-video-vesa is required, and if other open source drivers should be installed on a
     # per-target-hardware basis
     install_package mesa xf86-video-vesa
+
+# Bluetooth
+    install_package bluez blueman
+    sudo systemctl enable bluetooth.service
+
+
