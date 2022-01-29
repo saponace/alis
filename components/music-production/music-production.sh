ff9a1f6909a83895ab88e13d11bd81bac945ec21
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2022-01-29 09:39:35 -0500

    fix: copy udev rule instead of linking it. Udev couldn't access the linked file during init

--- components/music-production/music-production.sh
+++ components/music-production/music-production.sh
@@ -10,7 +10,9 @@
 # Configue bitwig desktop entry to start with pipewire jack bridge. This desktop file has to have the same name as the
 # one creatd in /usr/share/applications in order to override it
     create_link components/music-production/config/com.bitwig.BitwigStudio.desktop ${USER_HOMEDIR_DOTFILES_DESTINATION}/.local/share/applications/
+
 # Add user to audio group
     sudo usermod -a -G audio ${USERNAME}
-# Give user write permission on Ableton push USB device
-    create_link components/music-production/config/10-ableton-push.rules /etc/udev/rules.d/
+
+# Give user write permission on Ableton push 2 USB device
+    sudo cp components/music-production/config/10-ableton-push.rules /etc/udev/rules.d/
