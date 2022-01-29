b9ed1d2ffff006d752a2120c7f5722e420686a2b
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2022-01-29 07:00:46 -0500

    fix: fix resolv.con target link location

--- components/networking/networking.sh
+++ components/networking/networking.sh
@@ -8,4 +8,4 @@
     # Disable NetworkManager /etc/resolv.conf DNS automatic updates and manually set custom DNS servers
     # If not disabled; NetworkManager will set arbitrary DNS (notably ISP DNS)
         sudo su -c "echo -e '[main]\ndns=none' >> /etc/NetworkManager/NetworkManager.conf"
-        create_link components/networking/config/resolv.conf /etc/resolv.conf_static_manual_fallback
+        create_link components/networking/config/resolv.conf /etc
