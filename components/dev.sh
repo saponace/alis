0de3655d0d796fee0b4ff2d0c7ad0dc88eed08fb
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2022-02-09 11:08:23 -0500

    feat: add git setup

--- components/dev.sh
+++ components/dev.sh
@@ -13,3 +13,12 @@
 # IDEs
     install_package intellij-idea-ultimate-edition
     create_manual_todo 'Development' 'Log in with Jetbrains account in app "Intellij IDEA"'
+
+# git
+    # Generate ssh keypair
+        ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
+    # Set remote connection to this repository as SSH instead of HTTPS
+    git remote set-url origin git@github.com:saponace/alis.git
+    create_manual_todo 'Git' 'Log into github.com and add a new SSH key (get the public key with `cat  ~/.ssh/id_rsa.pub | xclip`)'
+    create_manual_todo 'Git' 'Set default git default user name and email address with `git config --global user.email "EMAIL_ADDRESS"` and `git config --global user.name "FIRST_NAME LAST_NAME"`'
+
