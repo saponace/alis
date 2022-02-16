4731522e7f49a3219988712d81c6e3a8dbea3885
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2022-02-16 17:21:41 -0500

    feat: windows-style international keyboard with dead keys

--- components/system/system.sh
+++ components/system/system.sh
@@ -6,3 +6,7 @@
 # Synchronize clock with NTP time servers
     install_package ntp
     sudo systemctl enable ntpd.service
+
+
+# Windows-style international keyboard dead keys
+    create_link components/system/config/.XCompose ${USER_HOMEDIR_DOTFILES_DESTINATION}
