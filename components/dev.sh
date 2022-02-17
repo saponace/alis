d7e2728d0f9fbbc7b7dc76beb9aa904f4ebccf7e
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2022-02-17 13:53:42 -0500

    fix: set java 11 as default JDK (Intellij IDEA couldn't start from Java 17)

--- components/dev.sh
+++ components/dev.sh
@@ -4,10 +4,11 @@
 
 
 # Java Development Kit
-    # Installing this JDK first will set it qs default JDK
     install_package jdk-openjdk openjdk-doc openjdk-src
     install_package jdk11-openjdk openjdk11-doc openjdk11-src
     install_package jdk8-openjdk openjdk8-doc openjdk8-src
+    # Set default Java version to 11
+        sudo archlinux-java set java-11-openjdk
 # Java build tools
     install_package maven
 # IDEs
