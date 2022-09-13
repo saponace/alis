0838331c38ba4208a6218af7a4d3fa5894dfcc57
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2022-09-13 18:45:38 -0400

    feat: add Solaar (logitech devices manager)

--- components/desktop-apps.sh
+++ components/desktop-apps.sh
@@ -24,3 +24,5 @@
 # Transmission (torrenting) daemon (and CLI and web-UI)
     install_package transmission-cli
     transmission-remote-cli --create-config   # Make transmission detect config file
+# Solaar: Logitech devices manager
+    install_package solaar
