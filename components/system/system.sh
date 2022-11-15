22e832720168e6c09243d032a05e12295566bc29
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2022-11-14 23:20:35 -0500

    feat: add instructions to install realtime kernel

--- components/system/system.sh
+++ components/system/system.sh
@@ -7,6 +7,8 @@
     install_package ntp
     sudo systemctl enable ntpd.service
 
-
 # Windows-style international keyboard dead keys
     create_link components/system/config/.XCompose ${USER_HOME}
+
+# Realtime kernel
+    create_manual_todo 'System' 'Install realtime kernel with "sudo mhwd-kernel -i _KERNEL_VERSION_". Get version with "mhwd-kernel -l". Kernel used at startup is the latest (realtime or not). Make sure the highest installed kernel version is the realtime one. If not, uninstall other kernels.'
