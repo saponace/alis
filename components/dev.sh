bccd54d1a1ffb0355bdd8f5bf93b4704ef25c75d
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2024-07-29 22:32:44 -0400

    chore(dev): remove useless packages for game dev

--- components/dev.sh
+++ components/dev.sh
@@ -20,9 +20,3 @@ ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa # Generate ssh keypair
 git remote set-url origin git@github.com:saponace/alis.git
 create_manual_todo 'Git' 'Log into github.com and add a new SSH key (get the public key with `cat  ~/.ssh/id_rsa.pub | xclip`)'
 create_manual_todo 'Git' 'Set default git default user name and email address with `git config --global user.email "EMAIL_ADDRESS"` and `git config --global user.name "FIRST_NAME LAST_NAME"`'
-
-# Game dev
-${install} rider # Csharp IDE
-${install} unityhub
-${install} mono mono-tools msbuild
-create_manual_todo 'Development' 'Open "Unity hub" and install unity through it'
