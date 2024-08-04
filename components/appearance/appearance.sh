83e5308b2ee3e068e272ef9dcc845c272d6d486d
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2024-08-04 17:23:32 -0400

    fix(gtk): add GTK-4.0 configurations

--- components/appearance/appearance.sh
+++ components/appearance/appearance.sh
@@ -9,4 +9,5 @@ install_package ttf-hack-nerd #icons
 # GTK theme
 install_package arc-gtk-theme
 create_homedir_link components/appearance/config/gtk-3.0 .config
+create_homedir_link components/appearance/config/gtk-4.0 .config
 create_homedir_link components/appearance/config/.gtkrc-2.0 .
