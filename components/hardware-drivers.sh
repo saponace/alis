e4e12dff4d813b63340a5bb4cb429c6988be87af
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2024-12-14 12:12:11 -0500

    enforce intl keymap with dead keys

--- components/hardware-drivers.sh
+++ components/hardware-drivers.sh
@@ -14,5 +14,6 @@ sudo systemctl enable bluetooth.service
 create_finalize_startup_entry "Enable bluetooth" "dbus-update-activation-environment DISPLAY; blueman-applet &"
 
 # Keyboard remap and keybindings
+sudo localectl set-x11-keymap us pc105 intl
 create_finalize_startup_entry "Keyboard mapping: set keymap to US international (with dead keys, as configured in ~/.XCompose)" "setxkbmap us -variant intl"
 create_finalize_startup_entry "Keyboard mapping: Kill (and restart) X11 with Ctrl+Alt+Backspace" "setxkbmap -option terminate:ctrl_alt_bksp"
