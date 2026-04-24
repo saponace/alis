71eb6a4911e6b3868f509bc914b255ec812c15bb
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2026-04-24 17:04:25 -0400

    fix yay flags

--- common-functions.sh
+++ common-functions.sh
@@ -5,7 +5,7 @@
 
 function install_package() {
 	echo "Installing package(s): $@"
-	yay -S --noconfirm --needed --nocleanmenu --nodiffmenu --noremovemake $@
+	yay -S --noconfirm --needed --noremovemake --answerclean None --answerdiff None --answeredit None $@
 }
 
 ## Link a file and make sure the directory of the link exists
